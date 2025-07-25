8a0d42de546b5970e9dfeccaa91a9d852f5ac211
JoaPerusia <joaquinperusia@gmail.com>
2025-07-25 12:08:41 -0300

    Backup 25/7 - Vuelvo al backup del 23 con el archivo 'register' actualizado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,88 +1,97 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-label for="name" value="{{ __('Nombre') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            {{-- Espacio para el logo, si quieres añadir uno estático --}}
+            <div class="flex justify-center mb-4">
+                {{-- Puedes pegar aquí el contenido de tu application-logo.blade.php si es solo el SVG --}}
+                {{-- O usar una imagen: <img src="{{ asset('images/your-logo.png') }}" alt="Tu Logo" class="w-20 h-20"> --}}
+                {{-- Si no quieres logo, puedes dejar este div vacío o eliminarlo --}}
             </div>
 
-            <div class="mt-4">
-                <x-label for="apellido" value="{{ __('Apellido') }}" />
-                <x-input id="apellido" class="block mt-1 w-full" type="text" name="apellido" :value="old('apellido')" required autocomplete="apellido" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="dni" value="{{ __('DNI') }}" />
-                <x-input id="dni" class="block mt-1 w-full" type="text" name="dni" :value="old('dni')" required unique="users,dni" autocomplete="dni" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="fecha_nacimiento" value="{{ __('Fecha de Nacimiento') }}" />
-                <x-input id="fecha_nacimiento" class="block mt-1 w-full" type="date" name="fecha_nacimiento" :value="old('fecha_nacimiento')" required autocomplete="fecha_nacimiento" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="obra_social" value="{{ __('Obra Social') }}" />
-                <x-input id="obra_social" class="block mt-1 w-full" type="text" name="obra_social" :value="old('obra_social')" required autocomplete="obra_social" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="telefono" value="{{ __('Teléfono') }}" /> {{-- ¡AÑADIDO DE NUEVO! --}}
-                <x-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')" autocomplete="telefono" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Contraseña') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirmar Contraseña') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            {{-- Campo oculto para id_rol, por defecto 3 (Paciente) --}}
-            <input type="hidden" name="id_rol" value="3">
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
-
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-label>
+            {{-- Bloque de errores de validación, emulando <x-validation-errors> --}}
+            @if ($errors->any())
+                <div class="mb-4 font-medium text-red-600 dark:text-red-400">
+                    {{ __('¡Ups! Algo salió mal.') }}
                 </div>
+
+                <ul class="mt-3 list-disc list-inside text-sm text-red-600 dark:text-red-400">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('¿Ya registrado?') }}
-                </a>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                <x-button class="ms-4">
-                    {{ __('Registrar') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+                {{-- Nombre --}}
+                <div>
+                    <label for="name" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Nombre') }}</label>
+                    <input id="name" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
+                </div>
+
+                {{-- Apellido --}}
+                <div class="mt-4">
+                    <label for="apellido" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Apellido') }}</label>
+                    <input id="apellido" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="apellido" value="{{ old('apellido') }}" required autocomplete="apellido" />
+                </div>
+
+                {{-- DNI --}}
+                <div class="mt-4">
+                    <label for="dni" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('DNI') }}</label>
+                    <input id="dni" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="dni" value="{{ old('dni') }}" required autocomplete="dni" />
+                </div>
+
+                {{-- Fecha de Nacimiento --}}
+                <div class="mt-4">
+                    <label for="fecha_nacimiento" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Fecha de Nacimiento') }}</label>
+                    <input id="fecha_nacimiento" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="date" name="fecha_nacimiento" value="{{ old('fecha_nacimiento') }}" required autocomplete="fecha_nacimiento" />
+                </div>
+
+                {{-- Obra Social --}}
+                <div class="mt-4">
+                    <label for="obra_social" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Obra Social') }}</label>
+                    <input id="obra_social" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="obra_social" value="{{ old('obra_social') }}" required autocomplete="obra_social" />
+                </div>
+
+                {{-- Teléfono --}}
+                <div class="mt-4">
+                    <label for="telefono" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Teléfono') }}</label>
+                    <input id="telefono" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="telefono" value="{{ old('telefono') }}" autocomplete="telefono" />
+                </div>
+
+                {{-- Email Address --}}
+                <div class="mt-4">
+                    <label for="email" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Email') }}</label>
+                    <input id="email" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="email" name="email" value="{{ old('email') }}" required autocomplete="username" />
+                </div>
+
+                {{-- Password --}}
+                <div class="mt-4">
+                    <label for="password" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Contraseña') }}</label>
+                    <input id="password" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="password" name="password" required autocomplete="new-password" />
+                </div>
+
+                {{-- Confirm Password --}}
+                <div class="mt-4">
+                    <label for="password_confirmation" class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ __('Confirmar Contraseña') }}</label>
+                    <input id="password_confirmation" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
+
+                {{-- Campo oculto para id_rol, por defecto 3 (Paciente) --}}
+                <input type="hidden" name="id_rol" value="3">
+
+                
+                <div class="flex items-center justify-end mt-4">
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                        {{ __('¿Ya estás registrado?') }}
+                    </a>
+
+                    <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-4">
+                        {{ __('Registrar') }}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-guest-layout>
